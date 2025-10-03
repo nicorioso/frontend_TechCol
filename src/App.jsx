a2eac210c5ffb15bb1070a31e8319a6b3221baad
caceres90 <caceresmartinezjhonalexaner@gmail.com>
2025-10-02 22:25:34 -0500

    Se añadio formulario de inicio de sesión y registro

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,13 @@
 import { useState } from 'react'
-import ContactForm from './layouts/contact_form.jsx'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+import LoginForm from './layouts/login_form.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <ContactForm/>
-
+    <LoginForm/>
   )
 }
 
